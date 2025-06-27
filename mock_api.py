9dbf0b0dc60e39aa1ddd527e47390b7853a10ff9
Felipe Cunha <lfcunha.ti@gmail.com>
2025-06-27 01:15:51 -0300

    feat: criação de mock para poder validar a aplicação

---
+++ mock_api.py
@@ -0,0 +1,45 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from datetime import datetime
+import time
+import random
+
+class CotacaoMockHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        now = datetime.now().isoformat()
+
+        if self.path == "/cotacao/dolar":
+            data = {
+                "moeda": "dólar",
+                "sigla": "USD",
+                "valor": 5.80,
+                "momento_cotacao": now
+            }
+
+        elif self.path == "/cotacao/euro":
+            time.sleep(round(random.uniform(0.9, 4.9),2))  # Atraso de até 5 segundos para simular latência
+            data = {
+                "moeda": "euro",
+                "sigla": "EUR",
+                "valor": 5.73,
+                "momento_cotacao": now
+            }
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+def run_mock_server():
+    server_address = ("localhost", 3000)
+    httpd = HTTPServer(server_address, CotacaoMockHandler)
+    print("🔧 Mock da API rodando em http://localhost:3000")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run_mock_server()
